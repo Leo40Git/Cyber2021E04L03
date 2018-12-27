68c820f1e9e45f4f1a749c9ea83a281c40442005
Kfir Awad <kfir40mailminer@gmail.com>
2018-12-27 09:09:15 +0200

    programming

--- app/src/main/java/com/leo/cyber2021e04l03/MainActivity.java
+++ app/src/main/java/com/leo/cyber2021e04l03/MainActivity.java
@@ -2,12 +2,25 @@ package com.leo.cyber2021e04l03;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+        editText = findViewById(R.id.editText);
+    }
+
+    public void onClick_button(View view) {
+        String st = editText.getText().toString();
+        textView.setText(st);
     }
 }
